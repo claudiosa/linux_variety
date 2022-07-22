64a78b7b0cc2f3a7431d6279551a1055b8cd7b1b
Claudio Cesar de Sá <ccs1664@gmail.com>
2022-07-22 20:25:44 -0300

    updates

--- armbian/rupa_DIAMOND_A9_S912_2G/conky_start.sh
+++ armbian/rupa_DIAMOND_A9_S912_2G/conky_start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sleep 10 && conky;
+sleep 10 && conky --config=  /home/ccs/.config/conky/conky.conf ;
+#-c | --config= FILE
+#              Config file to load instead of ~/.config/conky/conky.conf.
